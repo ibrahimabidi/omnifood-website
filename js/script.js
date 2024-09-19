3c532b129331dcdcfcdc071445b59ad9cb627d3d
ibrahim abidi <ibrahimabidi08@gmail.com>
2024-09-19 18:55:44 +0100

    implementing a sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -43,6 +43,31 @@ allLinks.forEach(function (link) {
   });
 });
 
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
